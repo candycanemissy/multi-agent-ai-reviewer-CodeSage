43f9956266a71795a384012fd41a724096ddfbbe
Divya Arjarapu <divyaarjara@gmail.com>
2026-05-31 07:01:16 -0700

    Add frontend debugging

--- frontend/app.js
+++ frontend/app.js
@@ -15,27 +15,42 @@ async function analyze() {
     addMessage(code, "user");
 
     addMessage("AI is analyzing code...", "ai");
- try {
-    const API_URL =
-      "https://multi-agent-ai-reviewer-codesage.onrender.com/analyze";
 
-    const res = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ code })
-    });
+    try {
 
-    const data = await res.json();
+        const API_URL =
+          "https://multi-agent-ai-reviewer-codesage.onrender.com/analyze";
 
-    chat.lastChild.remove();
+        const res = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ code })
+        });
 
-    addMessage("🔍 REVIEW:\n" + data.review, "ai");
-    addMessage("🛠 FIXED CODE:\n" + data.fixed_code, "ai");
-    addMessage("📖 EXPLANATION:\n" + data.explanation, "ai");
+        console.log("Status:", res.status);
 
-} catch (err) {
-    chat.lastChild.remove();
-    addMessage("❌ Error: " + err.message, "ai");
-}}
+        const data = await res.json();
+
+        console.log("Response:", data);
+
+        if (!res.ok) {
+            throw new Error(JSON.stringify(data));
+        }
+
+        chat.lastChild.remove();
+
+        addMessage("🔍 REVIEW:\n" + data.review, "ai");
+        addMessage("🛠 FIXED CODE:\n" + data.fixed_code, "ai");
+        addMessage("📖 EXPLANATION:\n" + data.explanation, "ai");
+
+    } catch (err) {
+
+        console.error("Frontend Error:", err);
+
+        chat.lastChild.remove();
+
+        addMessage("❌ Error: " + err.message, "ai");
+    }
+}
